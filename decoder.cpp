b674c2d3a225dacfd2d9f9e5f7acc0c0a95dde59
Bercan aydin <143941941+maymun1234@users.noreply.github.com>
2026-07-31 14:48:21 +0300

    first commit

---
+++ decoder.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+
+
+//ECUbufferdan okunan değer
+struct CAN_Frame {
+    uint32_t id;
+    uint8_t dlc;
+    uint8_t data[8];
+};
+
+
+class ECU_Decoder {
+    private:
+    const uint32_t ENGINE_MSG_ID = 0X1A6;
+    const int MAX_SAFE_RPM= 6000;
+
+
+    void trigger_fuel_cut() {
+        std::cout << "FUEL CUT TRIGGERED" << std::endl;
+    }
+
+
+    public:
+    void process_frame(const CAN_Frame& frame){
+        if(frame.id != ENGINE_MSG_ID) return;
+      
+        uint16_t raw_rpm = (frame.data[2] << 8) | frame.data[3];
+
+        uint8_t speed = frame.data[4];
+        int temperature = frame.data[5] - 40;
+
+        std::cout << " (ID: 0x" << std::hex << frame.id << std::dec << ")\n";
+        std::cout << "   Devir: " << raw_rpm << " RPM\n";
+        std::cout << "   Hz:   " << (int)speed << " km/h\n";
+        std::cout << "   Sicaaklik: " << temperature << " C\n";
+
+
+        if (raw_rpm > MAX_SAFE_RPM) {
+            std::cout << "   [UYARI] Motor Redline sinirini asti\n";
+            trigger_fuel_cut();
+        }
+
+        std::cout << "-------------------------------------------\n";
+    }
+};
+
+
+
+int main(){
+    std::cout << "=== ECU CAN-BUS started ===\n\n";
+
+
+    ECU_Decoder decoder;
+
+
+    std::vector<CAN_Frame> hardware_buffer = {
+        {0x1A6, 8, {0x00, 0x00, 0x09, 0xC4, 0x46, 0x82, 0x00, 0x00}},
+        
+    };
+
+
+    for(const auto& frame : hardware_buffer){
+        decoder.process_frame(frame);
+    }
+    return 0;
+}
